49f2c8c9c9d2321e41bcde734535f8fc2dacbb00
Mikolaj Kaminski <mikolaj.kaminski@student.stenden.com>
2017-05-09 13:10:26 +0200

    Initial check-in of module HelloWorld

---
+++ HelloWorld/GreetPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+	public partial class GreetPage : ContentPage
+	{
+		public GreetPage()
+		{
+			InitializeComponent();
+		}
+
+		void Handle_Clicked(object sender, System.EventArgs e)
+		{
+			DisplayAlert("HOLY FUCK", "Kurwa kurwa kurwa", "GTFO");
+		}
+	}
+} 
